3186e911d5ef3bfd462038e5d5d1e731a3e5236f
w-olivine29 <w.olivine29@gmail.com>
2025-05-21 17:20:46 +0900

    feat: [ch02variable] - 변수타입

---
+++ lecture-practice/java-practice/src/ch02variable/Var02Type.java
@@ -0,0 +1,26 @@
+package ch02variable;
+
+public class Var02Type {
+    public static void main(String[] args) {
+
+        int i = 100;
+        double d1 = 100;
+        double d2 = 100.0;
+
+        boolean b = false; // boolean: true or false
+
+        System.out.println(" ==================== char 문자 ====================");
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = 100;
+        System.out.println("'c1': " + c1);
+        System.out.println("'c2': " + c2);
+        System.out.println("'c3': " + c3);
+
+
+        System.out.println(" ==================== String 문자열 ====================");
+        String str1 = "Java";
+    }
+
+
+}

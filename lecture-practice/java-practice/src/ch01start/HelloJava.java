5be9fbc230c3cb325e1e559b714f78b0915e8183
w-olivine29 <w.olivine29@gmail.com>
2025-05-21 14:14:32 +0900

    feat: [ch01start] - HelloJava.java 추가

---
+++ lecture-practice/java-practice/src/ch01start/HelloJava.java
@@ -0,0 +1,15 @@
+package ch01start;
+
+public class HelloJava {
+    public static void main(String[] args) { // main: 프로그램의 시작점
+        //System.out.println("Long time no see.");
+
+
+       /* ctrl + shift + /
+       System.out.println("Long time no see.");
+        System.out.println("Long time no see.");*/
+
+        System.out.println("Long time no see.");
+
+    }
+}

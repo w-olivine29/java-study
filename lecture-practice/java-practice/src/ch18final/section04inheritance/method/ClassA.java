52991be51cc1255f3cb6dcb150e5c98eebd493be
w-olivine29 <w.olivine29@gmail.com>
2025-05-28 02:27:56 +0900

    feat: [ch18final & ch19inheritance] 상속 & final - final 메서드

---
+++ lecture-practice/java-practice/src/ch18final/section04inheritance/method/ClassA.java
@@ -0,0 +1,8 @@
+package ch18final.section04inheritance.method;
+
+public class ClassA {
+
+    final public void printA() {
+        System.out.println("A");
+    }
+}
---
+++ lecture-practice/java-practice/src/ch18final/section04inheritance/method/ClassB.java
@@ -0,0 +1,9 @@
+package ch18final.section04inheritance.method;
+
+public class ClassB extends ClassA {
+
+    // printA()' cannot override 'printA()' in overridden method is final
+/*    public void printA() {
+        System.out.println("A");
+    }*/
+}

471748005ef557d9011adea816443abc73843e4e
w-olivine29 <w.olivine29@gmail.com>
2025-06-30 18:39:44 +0900

    feat: [Concurrency-ch1thread] 실습용 Logger 클래스 생성

---
+++ lecture-practice/concurrency/src/util/MyLogger.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+// 스레드 학습 시에 사용할 유틸클래스
+public abstract class MyLogger {
+
+    // 현재 시간을 원하는 포맷으로 출력하기 위함
+    private static final DateTimeFormatter formatter
+             = DateTimeFormatter.ofPattern("HH:mm:ss.SSS"); //시, 분, 초 , 미리초
+
+    public static void log(Object obj) {
+         String time = LocalTime.now().format(formatter);
+
+         //현재 시간, 현재스레드 이름, 객체
+        System.out.printf("%s [%9s] %s\n", time, Thread.currentThread().getName(), obj);
+    }
+}

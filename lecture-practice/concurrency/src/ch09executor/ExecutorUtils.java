7da47159e96ca6363e9ef7bb7571002bd0e9cfbe
w-olivine29 <w.olivine29@gmail.com>
2025-07-12 23:51:22 +0900

    feat: [Concurrency - ch9 스레드풀 & Executor 프레임워크] 스레드풀 상태 모니터링을 위한 ExecutorUtils 추가
    - ThreadPoolExecutor의 실행 상태를 출력하는 유틸리티 클래스
    - 풀 크기, 활성 스레드, 대기 중인 작업, 완료된 작업 수 표시
    - 스레드풀 디버깅 및 성능 모니터링에 활용

---
+++ lecture-practice/concurrency/src/ch09executor/ExecutorUtils.java
@@ -0,0 +1,28 @@
+package ch09executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static util.MyLogger.log;
+
+public abstract class ExecutorUtils {
+    public static void printState(ExecutorService executorService) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+
+            // 내부에 생산자-소비자 구조로 돼있고, pool에 작업을 던지면 thread가 바로 실행하는게 아니라 queue에 작업이 담김
+            int queuedTasks = poolExecutor.getQueue().size();
+
+            // 작업을 완료한 스레드 개수
+            long completedTask = poolExecutor.getCompletedTaskCount();
+
+            log(String.format(
+                    "[pool= %d, active= %d, queuedTasks= %d, completedTask= %d]",
+                    pool, active, queuedTasks, completedTask
+            ));
+        }else {
+            log(executorService);
+        }
+    }
+}

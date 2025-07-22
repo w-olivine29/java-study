701b2ae66472de5804a88c9674847ffa9fa0823d
w-olivine29 <w.olivine29@gmail.com>
2025-07-23 00:16:53 +0900

    Feat: [io-network-reflection] HTTP 서버만들기 (버전1 - 시작)
    - 기본적인 HTTP 서버 구현으로 웹브라우저 요청을 받아 HTTP 응답을 반환하는 기능 구현
    - TCP 소켓 기반으로 HTTP 프로토콜의 기본 구조(시작라인, 헤더, 바디)를 직접 구현하여 HTTP 통신 원리 학습
    - 단일 스레드로 순차적 요청 처리하며 브라우저 자동 요청인 favicon은 무시 처리
    - BufferedReader로 HTTP 요청 파싱, PrintWriter로 HTML 응답 생성 및 autoFlush 제어를 통한 버퍼링 처리 적용

---
+++ lecture-practice/io-network-reflection/src/ch06http/was/v1start/ServerMain1.java
@@ -0,0 +1,13 @@
+package ch06http.was.v1start;
+
+import java.io.IOException;
+
+public class ServerMain1 {
+
+    private static final int PORT = 12345;
+    public static void main(String[] args) throws IOException {
+
+        HttpServerV1 server = new HttpServerV1(PORT);
+        server.start();
+    }
+}

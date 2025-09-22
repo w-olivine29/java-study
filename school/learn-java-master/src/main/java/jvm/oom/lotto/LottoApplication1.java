0fece54f0854bd025a8ccdd7c21fe9f9413846f2
w-olivine29 <w.olivine29@gmail.com>
2025-09-22 23:13:18 +0900

    Feat:[learn-java-master] OOM 실습

---
+++ school/learn-java-master/src/main/java/jvm/oom/lotto/LottoApplication1.java
@@ -0,0 +1,20 @@
+package jvm.oom.lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//visualVM 으로 Heap 메모리 상태 모니터링
+public class LottoApplication1 {
+    private static final List<List<Integer>> lottoHistory = new ArrayList<>();
+
+    // OOM 발생 시뮬레이션
+    public static void main(String[] args) throws InterruptedException {
+        LottoGenerator generator = new LottoGenerator();
+        while (true) {
+            List<Integer> lottoNumbers = generator.generate();
+            System.out.println(lottoNumbers);
+            lottoHistory.add(lottoNumbers);
+
+        }
+    }
+}
